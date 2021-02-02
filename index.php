73863447d1f30ca00b3921e799d7a23fa0e5d64b
Alisson Linneker <hi@alisson.eng.br>
2021-02-02 16:55:19 -0300

    Go!
    First version

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+use PHPHtmlParser\Dom;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class b2bstack
+{
+    public $business;
+    public $dom;
+
+    public function __construct($business)
+    {
+        $this->business = $business;
+        $this->dom = new Dom;
+    }
+
+    public function reviews()
+    {
+        $pages = $this->pages();
+        $dom = $this->dom;
+
+        foreach ($pages as $page) {
+            $dom->loadStr($page);
+            foreach ($dom->find('.review-container') as $review) {
+
+                $dom->loadStr($dom->find('.ratings'));
+
+                foreach ($dom->find('.col-sm-12') as $x => $rating) {
+                    switch ($x) {
+                        case 0:
+                            $recommendation = substr_count($rating, "star_100");
+                            break;
+                        case 1:
+                            $costbenefit = substr_count($rating, "star_100");
+                            break;
+                        case 2:
+                            $usefacility = substr_count($rating, "star_100");
+                            break;
+                        case 3:
+                            $functionalities = substr_count($rating, "star_100");
+                            break;
+                        case 4:
+                            $support = substr_count($rating, "star_100");
+                            break;
+                    }
+                }
+
+                $dom->loadStr($review);
+                $reviews[explode('like-number-', $dom->find('.review-share span')[0]->getAttribute('class'))[1]] = [
+                    "reviewer" => [
+                        "name" => $dom->find('.name')->text,
+                        "relationship" => explode(': ', $dom->find('.positions span')[1]->text)[1],
+                        "avatar" => $dom->find('.reviewr img')->getAttribute('data-src')
+                    ],
+                    "title" => @$dom->find('.review-content h3')[0]->text,
+                    "content" => [
+                        "mostlike" => $dom->find('.review-content p')[6]->text,
+                        "leastlike" => $dom->find('.review-content p')[8]->text,
+                        "problemsolved" => $dom->find('.review-content p')[10]->text,
+                    ],
+                    "useful" => $dom->find('.review-share span')[0]->text,
+                    "link" => 'https://www.b2bstack.com.br/'.explode('https://www.b2bstack.com.br/', $dom->find('.review-content a')[0]->getAttribute('href'))[1],
+                    "starts" => [
+                        "recommendation" => $recommendation,
+                        "costbenefit" => $costbenefit,
+                        "usefacility" => $usefacility,
+                        "functionalities" => $functionalities,
+                        "support" => $support
+                    ]
+                ];
+            }
+        }
+
+        return $reviews;
+    }
+
+    private function pages()
+    {
+        $mc = JMathai\PhpMultiCurl\MultiCurl::getInstance();
+        $pages[] = $mc->addUrl('https://www.b2bstack.com.br/product/' . $this->business . '/avaliacoes')->response;
+
+        $dom = $this->dom;
+        $dom->loadStr($pages[0]);
+
+        $pages_count = count($dom->find('.pagination a'));
+        
+        for ($i = 1; $i < $pages_count; $i++)
+            $pages[] = $mc->addUrl('https://www.b2bstack.com.br/product/' . $this->business . '/avaliacoes?commit=++Popularidade+&order=upvoted&page=' . ($i + 1) . '&utf8=%E2%9C%93');
+        
+        foreach ($pages as $key => $page)
+            $return[] = ($key == 0 ? $page : $page->response);
+
+        //echo $mc->getSequence()->renderAscii(); // Output a call sequence diagram to see how the parallel calls performed.
+
+        return $return;
+    }
+}
+
+$reviews = new b2bstack("cpf-cnpj");
+echo "<pre>";
+print_r($reviews->reviews());
---
+++ vendor/composer/installed.php
@@ -0,0 +1,147 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '92cbdf628c859b3facd588f7557e74286e8ed2e8',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '92cbdf628c859b3facd588f7557e74286e8ed2e8',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.2.0',
+      'version' => '7.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0aa74dfb41ae110835923ef10a9d803a22d50e79',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60d379c243457e073cff02bc323a2a86cb355631',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53330f47520498c0ae1f61f7e2c90f55690c06a3',
+    ),
+    'jmathai/php-multi-curl' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ee0917264ab4ef15cf5b318a2dc7b4ac5e5847c7',
+    ),
+    'myclabs/php-enum' => 
+    array (
+      'pretty_version' => '1.7.7',
+      'version' => '1.7.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd178027d1e679832db9f38248fcc7200647dc2b7',
+    ),
+    'paquettg/php-html-parser' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e01a438ad5961cc2d7427eb9798d213c8a12629',
+    ),
+    'paquettg/string-encode' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8708e9fac9d5ddfc8fc2aac6004e2cd05d80fee',
+    ),
+    'php-http/httplug' => 
+    array (
+      'pretty_version' => '2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '191a0a1b41ed026b717421931f8d3bd2514ffbf9',
+    ),
+    'php-http/promise' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c4c1f9b7289a2ec57cde7f1e9762a5789506f88',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+  ),
+);
